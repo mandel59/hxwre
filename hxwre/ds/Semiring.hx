029d3b6bd196bd6961831d7b9a224e5305f2fa90
Ryusei Yamaguchi <mandel59@gmail.com>
2014-06-07 16:45:23 +0900

    remove extra public declarations

--- hxwre/ds/Semiring.hx
+++ hxwre/ds/Semiring.hx
@@ -1,9 +1,9 @@
 package hxwre.ds;
 
 typedef Semiring<T> = {
-    public var zero(get, never) : T;
-    public var one(get, never) : T;
+    var zero(get, never) : T;
+    var one(get, never) : T;
 
-    public function plus(a : T, b : T) : T;
-    public function times(a : T, b : T) : T;
+    function plus(a : T, b : T) : T;
+    function times(a : T, b : T) : T;
 }
